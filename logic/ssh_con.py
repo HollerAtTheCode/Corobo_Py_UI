f2a03bdf70d9f71026dce80a530a5958256e6359
Maximilian Holler <maxi.holler@freenet.de>
2019-07-19 12:24:02 +0200

    ssh and stuff #2

--- logic/ssh_con.py
+++ logic/ssh_con.py
@@ -1,6 +1,11 @@
 import paramiko
 
 ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect('10.10.10.10', username='niryo', password='robotics')
+stdin, stdout, stderr = ssh_client.exec_command('python ~/scripts/try.py A1')
 
-stdin, stdout, stderr = ssh_client.exec_command('python niryo_script_01 A2 A1')
+
+for line in stdout.read().splitlines():
+    print(line)
+
