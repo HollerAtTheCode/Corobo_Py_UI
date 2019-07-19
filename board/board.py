8f28876f0f6557aa154ec55a5b8abad748933e7a
mmetschl <M.Metschl@gmx.de>
2019-07-19 12:11:24 +0200

    refactoring field generation and comments

--- board/board.py
+++ board/board.py
@@ -8,6 +8,7 @@ from .field import Field
 class Board:
 
     def __init__(self, width, height):
+
         self.width = width
         self.height = height
         self.NumberOfRows = 13
@@ -30,50 +31,48 @@ class Board:
 
         for x in range(self.NumberOfColumns):
             for y in range(self.NumberOfRows):
-                # blue robot square
-                if 7 < x < 12 and y < 4:  # blue
-
+                # fields occupied by the robot
+                if 7 < x < 12 and y < 4:
                     self.generate_field(x, y, 1)
                 # first and second row of playarea
                 elif 2 < x < 17 and 5 < y < 8:
                     # random placement of forbidden fields in the playarea
-                    if random.randrange(1, 100) > 10: # green
-                        self.generate_field(x, y, 2)
-
-                    else:  # red
-                        self.generate_field(x, y, 3)
-
+                    self.generate_playarea_field(x, y)
                 # third row of playarea
                 elif 3 < x < 16 and y == 8:
-                    if random.randrange(1, 100) > 10: # green
-                        self.generate_field(x, y, 2)
-
-                    else:  # red
-                        self.generate_field(x, y, 3)
-
+                    self.generate_playarea_field(x, y)
                 # fourth row of playarea
-                elif 4 < x < 15 and y == 9:  # green
-                    if random.randrange(1, 100) > 10:
-                        self.generate_field(x, y, 2)
-
-                    else:  # red
-                        self.generate_field(x, y, 3)
-
+                elif 4 < x < 15 and y == 9:
+                    self.generate_playarea_field(x, y)
                 # fifth row of playarea
                 elif 6 < x < 13 and y == 10:  # green
-                    if random.randrange(1, 100) > 10:
-                        self.generate_field(x, y, 2)
-
-                    else:  # red
-                        self.generate_field(x, y, 3)
-
+                    self.generate_playarea_field(x, y)
                 # area around the playable area
-                else:  # white
+                else:  # outside of playarea
                     self.generate_field(x, y, 0)
 
 
+    def generate_playarea_field(self, x, y):
+        '''
+        crates new field inside of the playarea
+        90% probability of a playable field
+        90% probability of a playable field
+        :return: none
+        '''
+        # 90% probability of a playable field
+        if random.randrange(1, 100) > 10:
+            self.generate_field(x, y, 2)
+        # blocked field
+        else:
+            self.generate_field(x, y, 3)
+
+
     def generate_field(self, x, y, type):
-        # generate IDs for all fields, like on  a chess board. The letters are horizontal, the numbers are vertical
+        '''
+        crates new field in the fields{} dictionary
+        The ID is generated like on a chess board, letters horizontal, numbers vertical
+        :return: none
+        '''
         alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
         ident = alphabet[x] + str(y)
         # add new field to the field array
@@ -83,6 +82,12 @@ class Board:
 
 
     def draw_board(self, win):
+        '''
+        draw method for the board
+        the background and playable fields are static
+        blocked fields are displayed above the playable fields
+        :return: none
+        '''
         # draw the background image
         win.blit(self.bg,(60,15))
         # draw the forbidden fields in the play area
