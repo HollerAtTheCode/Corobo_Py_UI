83e0a83de080d5a290071e18ed6f2de2004187c2
Maximilian Holler <maxi.holler@freenet.de>
2019-07-24 12:07:35 +0200

    Hotifx

--- game.py
+++ game.py
@@ -81,6 +81,7 @@ class Game:
                             self.robotLogic.setNextTile(current_Brick)
                             self.robotLogic.updatePrevBricks(self.playerLogic.prev_Bricks)
                             self.robotLogic.getNextFieldId()
+                            self.bricks[current_Brick.ident].setImage("yellow")
 
                         self.roundCounter += 1
 
